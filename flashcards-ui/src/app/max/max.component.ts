dc7140ce0eedd95ab382f54409471b7f14bbb45d
max-nano <max.gross.de@gmx.de>
2025-08-19 13:30:30 +0200

    new component: max

---
+++ flashcards-ui/src/app/max/max.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-max',
+  imports: [],
+  templateUrl: './max.component.html',
+  styleUrl: './max.component.css'
+})
+export class MAXComponent {
+
+}
