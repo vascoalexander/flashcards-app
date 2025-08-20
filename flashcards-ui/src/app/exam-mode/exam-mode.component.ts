3b4a0d99c1ce6e341de74b36379ec57887bd486c
vascoalexander <vascoalexander@gmail.com>
2025-08-20 15:41:33 +0200

    add select for timer

--- flashcards-ui/src/app/exam-mode/exam-mode.component.ts
+++ flashcards-ui/src/app/exam-mode/exam-mode.component.ts
@@ -1,21 +1,19 @@
 import { Component } from '@angular/core';
+import {Flashcard} from '../flashcard.model';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-exam-mode',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './exam-mode.component.html',
   styleUrl: './exam-mode.component.css'
 })
 export class ExamModeComponent {
 
   flashcards: Flashcard[] = [];
+  sets: Flashcard[] = [];
   selectedTime: string = '15';
 
   startExam() {
-    if (selectedTime === "per-question") {
-      totalTime = flashcards.length * 60; // Sekunden
-    } else {
-      totalTime = selectedTime * 60; // Minuten in Sekunden
-    }
   }
 }
