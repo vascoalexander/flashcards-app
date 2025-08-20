edffa39fe4ead6ff8166b01e43758b33a522aee9
max-nano <max.gross.de@gmx.de>
2025-08-20 14:17:43 +0200

    feat: basiclayout version2

--- flashcards-ui/src/app/app.component.ts
+++ flashcards-ui/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MatToolbar, MatToolbarRow],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
