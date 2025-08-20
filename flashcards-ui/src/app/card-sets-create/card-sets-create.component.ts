16cd942610b0d458cbb2110d42850a17a48fbaa4
Naceus <161336167+Naceus@users.noreply.github.com>
2025-08-20 10:17:14 +0200

    feat: compose cardsets create

---
+++ flashcards-ui/src/app/card-sets-create/card-sets-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-sets-create',
+  imports: [],
+  templateUrl: './card-sets-create.component.html',
+  styleUrl: './card-sets-create.component.css'
+})
+export class CardSetsCreateComponent {
+
+}
