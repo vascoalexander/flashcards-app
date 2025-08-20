09ecfbdc6c9d672028141491188a4958ebef6939
max-nano <max.gross.de@gmx.de>
2025-08-20 11:03:53 +0200

    basic layout test1

--- flashcards-ui/src/app/app.component.ts
+++ flashcards-ui/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatToolbar, MatToolbarRow],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+
+export class AppComponent
+{
   title = 'Flashcards UI';
 }
