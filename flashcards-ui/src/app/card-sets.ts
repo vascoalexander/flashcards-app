25708cf53f7774f11178fa0a0f235167218f2436
Naceus <161336167+Naceus@users.noreply.github.com>
2025-08-21 08:32:27 +0200

    fix: card sets

--- flashcards-ui/src/app/card-sets.ts
+++ flashcards-ui/src/app/card-sets.ts
@@ -1,3 +1,4 @@
+
 export interface CardSets {
     id: number;
     title: string;
--- flashcards-ui/src/app/card-sets/card-sets.component.ts
+++ flashcards-ui/src/app/card-sets/card-sets.component.ts
@@ -27,3 +27,4 @@ export class CardSetsComponent {
     this.sets$ = from(this.cardSetService.getAllSets());
   }
 }
+
