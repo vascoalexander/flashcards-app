aa53ab07b05c2030721d3896c5adafd06d41831b
Naceus <naco1606@gmail.com>
2025-08-23 12:41:55 +0200

    feat: delete dialog and fix create double Sets massage

---
+++ flashcards-ui/src/app/components/Flashcard-Sets/confirm-delete-dialog/confirm-delete-dialog.component.ts
@@ -0,0 +1,48 @@
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+
+
+export type ConfirmDeleteDialog = {
+  confirmText?: string;
+  cancelText?: string;
+  name?: string;
+  entity: 'Deck' | 'Karte' | 'Karten';
+  title?: string;
+  count?: number;
+  message?: string;
+
+}
+@Component({
+  selector: 'app-confirm-delete-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatIconModule],
+  templateUrl: './confirm-delete-dialog.component.html',
+  styleUrl: './confirm-delete-dialog.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConfirmDeleteDialogComponent {
+  constructor(private ref: MatDialogRef<ConfirmDeleteDialogComponent, boolean>, @Inject(MAT_DIALOG_DATA) public data: ConfirmDeleteDialog) { }
+
+
+  get defaultTitle(): string {
+    if (this.data.title) return this.data.title;
+    const label = this.data.count ? `${this.data.count} ${this.data.entity}` : this.data.entity;
+    if (this.data.entity === 'Karten') {
+      { return `Wollen Sie wirklich ${label} aus dem ${this.data.name} entfernen?`; };
+    }
+    if (this.data.entity === 'Karte') return `Wollen Sie wirklich ${label} aus dem Deck entfernen?`;
+    return `${label} löschen?`;
+  }
+
+  get defaultMessage(): string {
+    if (this.data.message) return this.data.message;
+    if (this.data.name) return `Wollen Sie wirklich ${this.data.name}  löschen?`;
+    return '';
+  }
+
+  close(result: boolean) {
+    this.ref.close(result);
+  }
+}
