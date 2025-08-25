db08dd1509829da1bc665d5c814f14c8fd389cd5
max-nano <max.gross.de@gmx.de>
2025-08-25 16:51:51 +0200

    css-style: Hauptseite designt mit mobile first Ansatz. Bietet kleine Grundstruktur.

--- flashcards-ui/src/app/app.component.ts
+++ flashcards-ui/src/app/app.component.ts
@@ -1,57 +1,18 @@
-import { Component, inject, signal, effect } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-
-import { FlashcardSetsService } from './flashcard-sets.service';
-import { Flashcard, FlashcardSet } from './flashcard.model';
-import { InfoPanelComponent, InfoItem } from './components/info-panel/info-panel.component';
-import { FlashcardsService } from './flashcards.service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, InfoPanelComponent,],
+  imports: [ RouterOutlet, MatToolbar, MatToolbarRow, MatDividerModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent
+{
   title = 'Flashcards UI';
-  private setssvc = inject(FlashcardSetsService);
-  private cardsvc = inject(FlashcardsService);
-
-  infoItems = signal<InfoItem[]>([]);
-
-  constructor() {
-    effect(() => {
-      this.setssvc.setsChanged(); 
-      this.cardsvc.cardsChanged(); 
-      this.loadStats();
-    });
-  }
-
-  private async loadStats() {
-    try {
-      const sets: FlashcardSet[] = await this.setssvc.getAllSets();
-      await this.cardsvc.getFlashcards();
-      const cards: Flashcard[] = this.cardsvc.flashcards().map(card => {
-        const stats = JSON.parse(localStorage.getItem(`flashcard-stats-${card.id}`) || '{}');
-        return { ...card, ...stats };
-      });
-
-      const seen = cards.filter(c => c.timesSeen && c.timesSeen > 0).length;
-
-      const correct = cards.filter(c => c.correctCount && c.correctCount > 0).length;
-
-      const accuracy = seen > 0 ? Math.round((correct / seen) * 100) : 0;
-
-      this.infoItems.set([
-        { icon: 'layers', label: 'Karten gesamt', value: cards.length.toString() },
-        { icon: 'collections_bookmark', label: 'Sets gesamt', value: sets.length.toString() },
-        { icon: 'visibility', label: 'Karten gesehen', value: seen.toString() },
-        { icon: 'check_circle', label: 'Richtig beantwortet', value: `${correct} (${accuracy}%)` }
-      ]);
-    }
-    catch (e) {
-      console.error('Stats laden fehlgeschlagen', e);
-    }
-  }
+  currentYear = new Date().getFullYear();
+  footerTitle = `© ${this.currentYear}`;
 }
 
