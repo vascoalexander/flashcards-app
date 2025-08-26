282e8153f4f999c06ef3e1bbb71deffe455b6807
max-nano <max.gross.de@gmx.de>
2025-08-26 09:05:30 +0200

    feat: 56-404-notfound Componente erstellt und getestet. Funktion bereit.

--- flashcards-ui/src/app/app.routes.ts
+++ flashcards-ui/src/app/app.routes.ts
@@ -12,31 +12,26 @@ import { CardEditComponent } from './components/card-edit/card-edit.component';
 import { QuizComponent } from './quiz/quiz.component';
 
 import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page-404/page-404.component';
 
 
 
 export const routes: Routes = [
 
 
-  { path: 'test', component: TestComponent },
+  // { path: 'test', component: TestComponent },
   { path: 'cards/:id', component: CardDetailComponent },
-  { path: 'cards', component: CardListComponent },
-  { path: 'create', component: CardCreateComponent },
-
-  {
-    path: 'sets',
-    component: CardSetsComponent,
-  },
-  { path: 'sets/create', component: EditSetComponent },
+  { path: 'cards', component: CardListComponent, title: 'Alle Karten' },
+  { path: 'sets', component: CardSetsComponent, title: 'Set verwalten' },
+  { path: 'sets/create', component: EditSetComponent, title: 'Set erstellen' },
   { path: 'sets/:id/edit', component: EditSetComponent },
   { path: 'sets/:id/organize', component: EditSetComponent },
-  { path: 'card/new', component: CardCreateComponent, canDeactivate: [checkoutGuard] },
+  { path: 'card/new', component: CardCreateComponent, title: 'Karte erstellen', canDeactivate: [checkoutGuard] },
 
   { path: 'cards/:id/edit', component: CardEditComponent, canDeactivate: [checkoutGuard] },
-  { path: 'quiz', component: QuizComponent },
-  //TODO: homepage und 404 routen müssen angepasst werden
-  { path: '', component: HomeComponent },
-  // { path: '**', redirectTo: 'cards' }
+  { path: 'quiz', component: QuizComponent, title: 'Quiz' },
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: '**', component: Page404Component, title: '404' }
 
 ];
 
---
+++ flashcards-ui/src/app/components/page-404/page-404.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-page-404',
+  imports: [],
+  templateUrl: './page-404.component.html',
+  styleUrl: './page-404.component.css'
+})
+
+
+export class Page404Component
+{
+
+}
