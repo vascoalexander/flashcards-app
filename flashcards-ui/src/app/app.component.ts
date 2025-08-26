504b35babe91ac9ab55633a9271d7badfff477ca
max-nano <max.gross.de@gmx.de>
2025-08-26 16:52:58 +0200

    design: Learn App mit CSS Design erweitert zB Card-Detail, Card-List, Create, einige mobile responsive Verbesserungen, Suchfunktion für card-list hinzugefügt.

--- flashcards-ui/src/app/app.component.ts
+++ flashcards-ui/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, MatToolbar, MatToolbarRow, MatDividerModule],
+  imports: [ RouterOutlet, MatToolbar, MatToolbarRow, MatDividerModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
